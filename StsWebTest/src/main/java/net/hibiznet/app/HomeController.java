cec1d90f4a4b4ba42917f9521840dc100efcba79
ismartkorea <ismartkorea01@gmail.com>
2023-08-17 00:41:36 +0900

    - 메모리 호출 추가.

--- StsWebTest/src/main/java/net/hibiznet/app/HomeController.java
+++ StsWebTest/src/main/java/net/hibiznet/app/HomeController.java
@@ -2,10 +2,13 @@ package net.hibiznet.app;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +21,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	MemoryRepository memoryRepository;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -34,6 +40,17 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	@RequestMapping(value = "/memory", method = RequestMethod.GET)
+	public String memoryRepository(Locale locale, Model model) {
+		
+		Map<String,Object> param = new HashMap<String,Object>();
+		param.put("set", "test123");
+		memoryRepository.save(param);
+		
+		
+		return "";
 	}
 	
 }
